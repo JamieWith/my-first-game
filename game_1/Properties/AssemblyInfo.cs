b54cf6eb4047246f71b8320a5e3a80fefaf0973f
Jamie <jamiewith16@gmail.com>
2018-01-30 16:12:30 +0000

    Name changed

--- game_1/Properties/AssemblyInfo.cs
+++ game_1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("game_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("game_1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e20f29c7-3e0f-47ed-80aa-894356fe8646")]
+[assembly: Guid("00a987c8-94ab-43b9-a48b-493e30db4b44")]
 
 // Version information for an assembly consists of the following four values:
 //
